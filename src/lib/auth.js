d4066630c6f87be0fee9de373532dfbdbc8d9562
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-19 22:22:01 +0600

    jwt codes in auth and auth-client

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
-import { betterAuth, jwt } from "better-auth";
+import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("BeyondHorizons");
